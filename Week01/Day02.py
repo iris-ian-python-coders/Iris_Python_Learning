d2b293d6565957f0a78323671e3a2496ac3b42aa
iris-ian-python-coders <115740582+iris-ian-python-coders@users.noreply.github.com>
2026-07-21 17:45:53 -0500

    Update Day02.py

--- Week01/Day02.py
+++ Week01/Day02.py
@@ -1,6 +1,5 @@
 #Tue Jul 21, 2026
 
-
 #get the name of the camp
 CampName = input("What is the name of your camp? ")
 
@@ -16,15 +15,14 @@ DaysLeft = NumDays - CurrentDay
 #Output the number of days left in camp
 print("There are {} days left in {}".format(DaysLeft, CampName))
 
-#Converting miles per hour to meters per second
 
+
+#Converting miles per hour to meters per second
 def convert(mph:float):
    meters_per_sec = float(mph * 0.44704)
    return meters_per_sec
 
-
 miles = float(input("How many miles per hour are you going: "))
-
 
 print(f"You are going ",convert(miles), "meters per second.")
 
@@ -34,7 +32,6 @@ print(f"You are going ",convert(miles), "meters per second.")
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 until = int(input("Enter the age you want to calculate to (how many years you have until x, enter x):"))
-
 
 AgeUntil = until - age
 if age < until:
